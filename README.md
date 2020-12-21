44c102069c918767c60639b30d1e77ac2b0c1fd8
sycured <60801403+sycured@users.noreply.github.com>
2020-12-21 13:20:16 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# oraclelinux-haskell-builder
+
+[![Build Status](https://travis-ci.com/sycured/oraclelinux-haskell-builder.svg?branch=main)](https://travis-ci.com/sycured/oraclelinux-haskell-builder)
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xe
+
+mkimg=$(buildah from docker.io/oraclelinux:8)
+buildah config --author='sycured' "$mkimg"
+buildah config --label Name='oraclelinux-haskell-builder' "$mkimg"
+buildah run "$mkimg" -- useradd -m hsk
+buildah run "$mkimg" -- dnf upgrade -y
+buildah run "$mkimg" -- dnf install glibc-langpack-en tar -y
+buildah config --env LANG="en_US.UTF8" --env LC_ALL="en_US.UTF8" "$mkimg"
+buildah run "$mkimg" -- bash -c "curl -sSL https://get.haskellstack.org/ | sh"
+buildah run "$mkimg" -- rm -rf /var/cache/dnf/*
+buildah config --user='hsk' "$mkimg"
+buildah config --workingdir='/home/hsk' "$mkimg"
+buildah commit --squash "$mkimg" "oraclelinux-haskell-builder"
+buildah rm "$mkimg"
