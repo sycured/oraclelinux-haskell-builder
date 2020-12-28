5d7aee45eb0018a889f03111c8c0e81af9eadaa6
sycured <60801403+sycured@users.noreply.github.com>
2020-12-28 18:16:52 -0500

    add gcc

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ buildah config --author='sycured' "$mkimg"
 buildah config --label Name='oraclelinux-haskell-builder' "$mkimg"
 buildah run "$mkimg" -- useradd -m hsk
 buildah run "$mkimg" -- dnf upgrade -y
-buildah run "$mkimg" -- dnf install glibc-langpack-en tar -y
+buildah run "$mkimg" -- dnf install glibc-langpack-en gcc tar -y
 buildah config --env LANG="en_US.UTF8" --env LC_ALL="en_US.UTF8" "$mkimg"
 buildah run "$mkimg" -- bash -c "curl -sSL https://get.haskellstack.org/ | sh"
 buildah run "$mkimg" -- rm -rf /var/cache/dnf/*
